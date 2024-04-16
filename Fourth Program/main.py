2e6574fbf167ed6c0407e196492d149e46ce04fd
Laiba-Ahmad <laib.khan.an@gmail.com>
2024-04-16 07:21:10 +0500

    a program to display the year is leap or not

--- Fourth Program/main.py
+++ Fourth Program/main.py
@@ -1,11 +1,5 @@
 year = eval(input("Enter a 4-digit year: "))
 
-
-# if year % 4 == 0 & year % 100 != 0 :
-#     print("The year ", year, " is a Leap year.")
-# else :
-#     print("The year ", year, " is not a Leap year.")
-
 if year % 4 == 0:
     print("The year ", year, " is a Leap year.")
 else:
